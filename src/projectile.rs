55f52ef20ece2a172501034eb1b552efafaf1460
Kim Goetzke <kbgoetzke@gmail.com>
2024-07-14 12:46:53 +0100

    Allow for different projectiles

--- src/projectile.rs
+++ src/projectile.rs
@@ -1,32 +1,31 @@
-use crate::player::{Player, SHOOTING_COOLDOWN};
+use crate::player::Player;
 use bevy::color::Color;
 use bevy::input::ButtonInput;
 use bevy::math::{Vec2, Vec3};
 use bevy::prelude::*;
-use bevy_rapier2d::dynamics::{AdditionalMassProperties, GravityScale, RigidBody, Velocity};
-use bevy_rapier2d::prelude::Collider;
-
-const MAX_LIFE_TIME: f32 = 0.4;
-const PROJECTILE_SPEED: f32 = 750.0;
-const PROJECTILE_COLOUR: Color = Color::hsl(0.59, 0.32, 0.52);
+use bevy_rapier2d::prelude::*;
 
 pub struct ProjectilePlugin;
 
 impl Plugin for ProjectilePlugin {
   fn build(&self, app: &mut App) {
-    app.add_systems(
-      FixedUpdate,
-      (projectile_shooting_system, projectile_life_time_system),
-    );
+    app
+      .add_systems(FixedUpdate, projectile_shooting_system)
+      .add_systems(Update, projectile_life_time_system);
   }
 }
 
-#[derive(Component)]
-struct Projectile {
+#[derive(Component, Clone)]
+pub(crate) struct Projectile {
+  speed: f32,
   life_time: f32,
+  max_life_time: f32,
+  cooldown: f32,
+  collider: Collider,
+  color: Color,
 }
 
-pub(crate) fn projectile_shooting_system(
+fn projectile_shooting_system(
   mut commands: Commands,
   keyboard_input: Res<ButtonInput<KeyCode>>,
   mut query: Query<(&mut Player, &Transform)>,
@@ -34,13 +33,24 @@ pub(crate) fn projectile_shooting_system(
   if let Ok((mut player, player_transform)) = query.get_single_mut() {
     if keyboard_input.pressed(KeyCode::Space) && player.shooting_cooldown <= 0.0 {
       let player_forward = player_transform.rotation * Vec3::Y;
-      let projectile_position = player_transform.translation + player_forward * 5.0;
+      let projectile_position = player_transform.translation + player_forward * 15.0;
+      let projectile = Projectile {
+        speed: 750.0,
+        life_time: 0.0,
+        max_life_time: 0.4,
+        cooldown: 0.1,
+        collider: Collider::cuboid(0.5, 2.5),
+        color: Color::hsl(0.59, 0.32, 0.52),
+      };
 
-      // Draw the projectile
+      // Reset the shooting cooldown
+      player.shooting_cooldown = projectile.cooldown;
+
+      // Spawn the projectile
       commands
         .spawn(SpriteBundle {
           sprite: Sprite {
-            color: PROJECTILE_COLOUR,
+            color: projectile.color,
             custom_size: Some(Vec2::new(1.0, 5.0)),
             ..default()
           },
@@ -51,32 +61,25 @@ pub(crate) fn projectile_shooting_system(
           },
           ..default()
         })
-        .insert(Projectile { life_time: 0.0 })
-        .insert(Collider::ball(1.0))
+        .insert(projectile.collider.clone())
         .insert(RigidBody::Dynamic)
+        .insert(ActiveEvents::COLLISION_EVENTS)
         .insert(GravityScale(0.0))
         .insert(AdditionalMassProperties::Mass(100.0))
         .insert(Velocity {
-          linvel: Vec2::new(player_forward.x, player_forward.y) * PROJECTILE_SPEED,
+          linvel: Vec2::new(player_forward.x, player_forward.y) * projectile.speed,
           angvel: 0.0,
-        });
-
-      // Reset the shooting cooldown
-      player.shooting_cooldown = SHOOTING_COOLDOWN;
+        })
+        .insert(projectile);
     }
   }
 }
 
-fn projectile_life_time_system(
-  mut commands: Commands,
-  time: Res<Time>,
-  mut query: Query<(Entity, &mut Projectile)>,
-) {
+fn projectile_life_time_system(mut commands: Commands, time: Res<Time>, mut query: Query<(Entity, &mut Projectile)>) {
   for (entity, mut projectile) in query.iter_mut() {
     projectile.life_time += time.delta_seconds();
 
-    // Despawn
-    if projectile.life_time > MAX_LIFE_TIME {
+    if projectile.life_time > projectile.max_life_time {
       commands.entity(entity).despawn();
     }
   }
