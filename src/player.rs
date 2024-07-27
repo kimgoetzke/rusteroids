63292f6224cee428dbb5ec418270067141d17fc6
Kim Goetzke <kbgoetzke@gmail.com>
2024-07-27 18:07:59 +0100

    Add spaceship exhaust particles

--- src/player.rs
+++ src/player.rs
@@ -4,6 +4,7 @@ use crate::game_state::GameState;
 use crate::game_world::WORLD_SIZE;
 use bevy::audio::Volume;
 use bevy::prelude::*;
+use bevy_enoki::prelude::{OneShot, ParticleSpawnerBundle, DEFAULT_MATERIAL};
 use bevy_rapier2d::dynamics::RigidBody;
 use bevy_rapier2d::prelude::*;
 
@@ -34,36 +35,38 @@ pub struct Player {
   pub shooting_cooldown: f32,
 }
 
+#[derive(Component)]
+pub struct ExhaustParticles;
+
 const MOVEMENT_SPEED: f32 = 125.0;
 
 fn spawn_player_system(mut commands: Commands, asset_server: Res<AssetServer>) {
   let player_handle = asset_server.load("player_base.png");
   let audio_handle = asset_server.load("audio/spaceship_loop_default.ogg");
 
-  commands
-    .spawn((
-      SpriteBundle {
-        texture: player_handle,
-        ..default()
-      },
-      PIXEL_PERFECT_LAYERS,
-    ))
-    .insert(Player {
+  commands.spawn((
+    SpriteBundle {
+      texture: player_handle,
+      ..default()
+    },
+    PIXEL_PERFECT_LAYERS,
+    Player {
       movement_speed: MOVEMENT_SPEED,
       rotation_speed: 5.0,
       shooting_cooldown: SHOOTING_COOLDOWN,
-    })
-    .insert(RigidBody::Dynamic)
-    .insert(Collider::ball(9.0))
-    .insert(ActiveEvents::COLLISION_EVENTS)
-    .insert(GravityScale(0.0))
-    .insert(Velocity {
+    },
+    Name::new("Player"),
+    RigidBody::Dynamic,
+    Collider::ball(9.0),
+    ActiveEvents::COLLISION_EVENTS,
+    GravityScale(0.0),
+    Velocity {
       linvel: Vec2::new(0.0, 25.0),
       angvel: 0.0,
-    })
-    .insert(AdditionalMassProperties::Mass(2.0))
-    .insert(Ccd::enabled())
-    .insert(AudioBundle {
+    },
+    AdditionalMassProperties::Mass(2.0),
+    Ccd::enabled(),
+    AudioBundle {
       source: audio_handle,
       settings: PlaybackSettings {
         mode: bevy::audio::PlaybackMode::Loop,
@@ -71,17 +74,20 @@ fn spawn_player_system(mut commands: Commands, asset_server: Res<AssetServer>) {
         speed: 0.3,
         ..Default::default()
       },
-    })
-    .insert((SpatialListener::new(10.0), SpatialBundle::default()));
+    },
+    SpatialListener::new(10.0),
+  ));
 }
 
 fn player_movement_system(
   time: Res<Time>,
   keyboard_input: Res<ButtonInput<KeyCode>>,
-  mut query: Query<(&mut Player, &Transform, &mut Velocity, &AudioSink)>,
+  mut player_query: Query<(&mut Player, &Transform, &mut Velocity, &AudioSink, Entity)>,
+  mut commands: Commands,
+  asset_server: Res<AssetServer>,
 ) {
-  for (player, transform, mut velocity, audio_sink) in query.iter_mut() {
-    let mut play_audio = false;
+  for (player, transform, mut velocity, audio_sink, player_entity) in player_query.iter_mut() {
+    let mut is_moving = false;
 
     // Update rotation
     let rotation_factor = if keyboard_input.pressed(KeyCode::KeyA) {
@@ -93,16 +99,32 @@ fn player_movement_system(
     };
     velocity.angvel = rotation_factor * player.rotation_speed;
 
-    // Set acceleration and turn on audio
+    // Set acceleration and spawn particles if moving
     if keyboard_input.pressed(KeyCode::KeyW) {
+      is_moving = true;
       let direction = transform.rotation * Vec3::Y;
       let acceleration = Vec2::new(direction.x, direction.y) * player.movement_speed;
       velocity.linvel += acceleration * time.delta_seconds();
-      play_audio = true;
+      commands.entity(player_entity).with_children(|builder| {
+        builder.spawn((
+          ParticleSpawnerBundle {
+            effect: asset_server.load("particles/spaceship_exhaust.ron"),
+            material: DEFAULT_MATERIAL,
+            transform: Transform {
+              translation: Vec3::new(0.0, 0.0, -50.0),
+              scale: Vec3::splat(0.3),
+              ..default()
+            },
+            ..default()
+          },
+          OneShot::Despawn,
+          ExhaustParticles,
+        ));
+      });
     }
 
     // Update volume if it has changed
-    if play_audio == audio_sink.is_paused() {
+    if is_moving == audio_sink.is_paused() {
       audio_sink.toggle();
     }
 
