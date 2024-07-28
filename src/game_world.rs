727c128bcb2f6d1e84ea848a41d4255f88d86f16
Kim Goetzke <kbgoetzke@gmail.com>
2024-07-28 05:48:10 +0100

    Add bulkypix

--- src/game_world.rs
+++ src/game_world.rs
@@ -1,11 +1,11 @@
 use crate::camera::PIXEL_PERFECT_LAYERS;
-use crate::shared::{BLACK, DARK_GRAY};
+use crate::shared::{BLACK, DARK_GRAY, DEFAULT_FONT};
 use bevy::prelude::*;
 use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
 
-pub(crate) const WORLD_SIZE: f32 = 1000.0;
-const TILES: f32 = 5.0; // Must result in a whole number when dividing by WORLD_SIZE
-const MARGIN: f32 = 2.0; // Must be divisible by 2
+pub(crate) const WORLD_SIZE: f32 = 1000.;
+const TILES: f32 = 5.; // Must result in a whole number when dividing by WORLD_SIZE
+const MARGIN: f32 = 2.; // Must be divisible by 2
 
 pub struct GameWorldPlugin;
 
@@ -19,22 +19,23 @@ fn create_world_system(
   mut commands: Commands,
   mut meshes: ResMut<Assets<Mesh>>,
   mut materials: ResMut<Assets<ColorMaterial>>,
+  asset_server: Res<AssetServer>,
 ) {
   let tile_size = WORLD_SIZE / TILES;
-  let half_world = WORLD_SIZE / 2.0;
-  let half_margin = MARGIN / 2.0;
+  let half_world = WORLD_SIZE / 2.;
+  let half_margin = MARGIN / 2.;
   let adjusted_tile_size = tile_size - MARGIN;
 
   for i in 0..TILES as i32 {
     for j in 0..TILES as i32 {
-      let x = (i as f32 * tile_size) - half_world + (tile_size / 2.0);
-      let y = (j as f32 * tile_size) - half_world + (tile_size / 2.0);
+      let x = (i as f32 * tile_size) - half_world + (tile_size / 2.);
+      let y = (j as f32 * tile_size) - half_world + (tile_size / 2.);
 
       commands
         .spawn((
           MaterialMesh2dBundle {
             mesh: Mesh2dHandle(meshes.add(Rectangle::new(adjusted_tile_size, adjusted_tile_size))),
-            transform: Transform::from_xyz(x + half_margin, y + half_margin, -999.0),
+            transform: Transform::from_xyz(x + half_margin, y + half_margin, -999.),
             material: materials.add(BLACK),
             ..default()
           },
@@ -45,15 +46,16 @@ fn create_world_system(
           builder.spawn((
             Text2dBundle {
               text: Text::from_section(
-                format!("{}|{}", i, j),
+                format!("{}/{}", i, j),
                 TextStyle {
-                  font_size: 20.0,
+                  font: asset_server.load(DEFAULT_FONT),
+                  font_size: 20.,
                   color: DARK_GRAY,
                   ..default()
                 },
               )
               .with_justify(JustifyText::Center),
-              transform: Transform::from_xyz(0., 0., 1.0),
+              transform: Transform::from_xyz(0., 0., 1.),
               ..default()
             },
             PIXEL_PERFECT_LAYERS,
