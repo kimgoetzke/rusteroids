1d33516e0467579736b6d791966b890e2f030ecc
Kim Goetzke <kbgoetzke@gmail.com>
2024-08-29 21:20:02 +0100

    Clean up shared stuff

--- src/shared.rs
+++ src/shared.rs
@@ -10,37 +10,37 @@ use bevy_rapier2d::prelude::CollisionGroups;
 use rand::random;
 
 #[allow(dead_code)]
-pub const RED: Color = Color::hsl(0.59, 0.32, 0.52);
+pub(crate) const RED: Color = Color::hsl(0.59, 0.32, 0.52);
 #[allow(dead_code)]
-pub const PURPLE: Color = Color::srgb(0.706, 0.557, 0.678);
+pub(crate) const PURPLE: Color = Color::srgb(0.706, 0.557, 0.678);
 #[allow(dead_code)]
-pub const YELLOW: Color = Color::srgb(0.922, 0.796, 0.545);
+pub(crate) const YELLOW: Color = Color::srgb(0.922, 0.796, 0.545);
 #[allow(dead_code)]
-pub const BLUE: Color = Color::srgb(0.533, 0.753, 0.816);
+pub(crate) const BLUE: Color = Color::srgb(0.533, 0.753, 0.816);
 #[allow(dead_code)]
-pub const ORANGE: Color = Color::srgb(0.816, 0.529, 0.439);
+pub(crate) const ORANGE: Color = Color::srgb(0.816, 0.529, 0.439);
 #[allow(dead_code)]
-pub const GREEN: Color = Color::srgb(0.639, 0.745, 0.549);
+pub(crate) const GREEN: Color = Color::srgb(0.639, 0.745, 0.549);
 #[allow(dead_code)]
-pub const LIGHT_1: Color = Color::srgb(0.925, 0.937, 0.957);
+pub(crate) const LIGHT_1: Color = Color::srgb(0.925, 0.937, 0.957);
 #[allow(dead_code)]
-pub const LIGHT_2: Color = Color::srgb(0.898, 0.914, 0.941);
+pub(crate) const LIGHT_2: Color = Color::srgb(0.898, 0.914, 0.941);
 #[allow(dead_code)]
-pub const LIGHT_3: Color = Color::srgb(0.847, 0.871, 0.914);
+pub(crate) const LIGHT_3: Color = Color::srgb(0.847, 0.871, 0.914);
 #[allow(dead_code)]
-pub const MEDIUM_1: Color = Color::srgb(0.60, 0.639, 0.714);
+pub(crate) const MEDIUM_1: Color = Color::srgb(0.60, 0.639, 0.714);
 #[allow(dead_code)]
-pub const MEDIUM_2: Color = Color::srgb(0.427, 0.478, 0.588);
+pub(crate) const MEDIUM_2: Color = Color::srgb(0.427, 0.478, 0.588);
 #[allow(dead_code)]
-pub const DARK_1: Color = Color::srgb(0.298, 0.337, 0.416);
+pub(crate) const DARK_1: Color = Color::srgb(0.298, 0.337, 0.416);
 #[allow(dead_code)]
-pub const DARK_4: Color = Color::srgb(0.18, 0.204, 0.251);
+pub(crate) const DARK_4: Color = Color::srgb(0.18, 0.204, 0.251);
 #[allow(dead_code)]
-pub const VERY_DARK_1: Color = Color::srgb(0.12, 0.14, 0.18);
+pub(crate) const VERY_DARK_1: Color = Color::srgb(0.12, 0.14, 0.18);
 #[allow(dead_code)]
-pub const VERY_DARK_2: Color = Color::srgb(0.06, 0.07, 0.09);
+pub(crate) const VERY_DARK_2: Color = Color::srgb(0.06, 0.07, 0.09);
 
-pub const DEFAULT_FONT: &str = "fonts/bulkypix.ttf";
+pub(crate) const DEFAULT_FONT: &str = "fonts/bulkypix.ttf";
 
 #[derive(Component, Clone, Debug)]
 pub(crate) struct ProjectileInfo {
@@ -180,21 +180,21 @@ impl fmt::Display for Category {
   }
 }
 
-pub fn random_f32_range(min: f32, max: f32) -> f32 {
+pub(crate) fn random_f32_range(min: f32, max: f32) -> f32 {
   (random::<f32>() * (max - min)) + min
 }
 
-pub fn random_u16_range(min: u16, max: u16) -> u16 {
+pub(crate) fn random_u16_range(min: u16, max: u16) -> u16 {
   random::<u16>() % (max - min) + min
 }
 
-pub fn random_game_world_point() -> Vec3 {
+pub(crate) fn random_game_world_point() -> Vec3 {
   let x = random_f32_range(-WORLD_SIZE / 2., WORLD_SIZE / 2.);
   let y = random_f32_range(-WORLD_SIZE / 2., WORLD_SIZE / 2.);
   Vec3::new(x, y, 0.)
 }
 
-pub fn random_game_world_point_away_from_player(player_position: Vec3, distance: f32) -> Vec3 {
+pub(crate) fn random_game_world_point_away_from_player(player_position: Vec3, distance: f32) -> Vec3 {
   let proposed_point = random_game_world_point();
   if (player_position - proposed_point).length() < distance {
     debug!(
@@ -207,7 +207,7 @@ pub fn random_game_world_point_away_from_player(player_position: Vec3, distance:
   }
 }
 
-pub fn random_game_world_point_close_to_origin_without_player_collision(
+pub(crate) fn random_game_world_point_close_to_origin_without_player_collision(
   origin: Vec3,
   proximity: f32,
   player_position: Vec3,
@@ -236,39 +236,39 @@ const ASTEROID_GROUP: Group = Group::GROUP_4;
 const ENEMY_GROUP: Group = Group::GROUP_5;
 const ENEMY_PROJECTILES_GROUP: Group = Group::GROUP_6;
 
-pub fn get_player_collision_groups() -> CollisionGroups {
+pub(crate) fn get_player_collision_groups() -> CollisionGroups {
   CollisionGroups::new(
     PLAYER_GROUP,
     POWER_UP_GROUP | ASTEROID_GROUP | ENEMY_GROUP | ENEMY_PROJECTILES_GROUP,
   )
 }
 
-pub fn player_projectile_collision_groups() -> CollisionGroups {
+pub(crate) fn player_projectile_collision_groups() -> CollisionGroups {
   CollisionGroups::new(
     FRIENDLY_PROJECTILES_GROUP,
     ASTEROID_GROUP | ENEMY_GROUP | ENEMY_PROJECTILES_GROUP,
   )
 }
 
-pub fn power_up_collision_groups() -> CollisionGroups {
+pub(crate) fn power_up_collision_groups() -> CollisionGroups {
   CollisionGroups::new(POWER_UP_GROUP, PLAYER_GROUP)
 }
 
-pub fn default_enemy_collision_groups() -> CollisionGroups {
+pub(crate) fn default_enemy_collision_groups() -> CollisionGroups {
   CollisionGroups::new(
     ENEMY_GROUP,
     PLAYER_GROUP | FRIENDLY_PROJECTILES_GROUP | ASTEROID_GROUP | ENEMY_GROUP,
   )
 }
 
-pub fn enemy_projectile_collision_groups() -> CollisionGroups {
+pub(crate) fn enemy_projectile_collision_groups() -> CollisionGroups {
   CollisionGroups::new(
     ENEMY_PROJECTILES_GROUP,
     Group::GROUP_1 | FRIENDLY_PROJECTILES_GROUP | ASTEROID_GROUP,
   )
 }
 
-pub fn asteroid_collision_groups() -> CollisionGroups {
+pub(crate) fn asteroid_collision_groups() -> CollisionGroups {
   CollisionGroups::new(
     ASTEROID_GROUP,
     PLAYER_GROUP | FRIENDLY_PROJECTILES_GROUP | ASTEROID_GROUP | ENEMY_GROUP | ENEMY_PROJECTILES_GROUP,
